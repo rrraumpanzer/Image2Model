0a7a8a6d5ee75a8609ae50c075e550b0b463f064
rrraumpanzer <ayylmao3713@gmail.com>
2024-11-06 13:55:35 +0300

    Working endpoints for images and coordinates. Good looking README

--- image2model/__main__.py
+++ image2model/__main__.py
@@ -1,39 +1,62 @@
 from fastapi import FastAPI, File, UploadFile
-from fastapi.responses import JSONResponse
-from typing import List
+from fastapi.responses import JSONResponse, Response
 from pydantic import BaseModel
 from PIL import Image
+from io import BytesIO
 app = FastAPI()
 
 class Coordinates(BaseModel):
     x: int
     y: int
 
-class Storage: #Хранение координат и (добавить?) изображения 
+class Storage: #Хранение координат и изображения 
     def __init__(self):
         self.coordinates = [512, 512]
+        self.image = None
 
-    def update(self, coords: Coordinates):
+    def set_coords(self, coords: Coordinates):
         self.coordinates = [coords.x, coords.y]
-
-    def get(self):
+    def get_coords(self):
         return self.coordinates
+    
+    def set_image(self, image: Image):
+        self.image = image
+    def get_image(self):
+        return self.image
+    
 
 img_coord_storage = Storage()
 
 @app.post('/image')
-def read_image(image: UploadFile = File(...)):
-  try:        
-      im = Image.open(image.file)
-      if im.mode in ("RGBA", "P"): 
-          im = im.convert("RGB")
-      im.save('image.jpg', 'JPEG')
-      return JSONResponse({'Successfully read an image'}, status_code=200)
-  except Exception:
-      return JSONResponse({'Something went wrong'}, status_code=500)
-  finally:
-      image.file.close()
-      im.close()
+async def post_image(image: UploadFile = File(...)):
+    try:        
+        image_bytes = await image.read()
+        im = Image.open(BytesIO(image_bytes))
+        if im.mode in ("RGBA", "P"): 
+            im = im.convert("RGB")
+        
+        #im.save('image.jpg', 'JPEG') если надо сохранить на диск
+        img_coord_storage.set_image(im)
+
+        return 'Successfully read an image'
+    except Exception as err:
+        return f'Error: {err}'
+
+@app.get('/image')
+async def read_image():
+    try:
+        image = img_coord_storage.get_image()
+        if image is None:
+            return JSONResponse(status_code=404)
+        
+        #Нельзя отправлять PIL напрямую, только через BytesIO и StreamingResponse, не знаю почему
+        img_bytes = BytesIO()
+        image.save(img_bytes, format="JPEG")
+        img_bytes.seek(0)
+
+        return Response(content=img_bytes.read(), media_type="image/jpeg")
+    except Exception as err:
+        return f'Error: {err}'
 
 @app.post('/coords')
 async def read_coords(coords: Coordinates):
