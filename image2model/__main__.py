3c0d7f5308125ce19f5b5017a8c59e306f8eb567
rrraumpanzer <ayylmao3713@gmail.com>
2024-11-11 20:46:34 +0300

    Добавлена логика обработки изображения по запросу. Изменен функционал под 4 координаты для модели, вместо 2, что были раньше. Обновлён README.rst объяснениями принципа работы и тестовыми запросами.

--- image2model/__main__.py
+++ image2model/__main__.py
@@ -3,19 +3,25 @@ from fastapi.responses import JSONResponse, Response
 from pydantic import BaseModel
 from PIL import Image
 from io import BytesIO
+from image2model.model import ImageSegmenter
 app = FastAPI()
 
+
+
 class Coordinates(BaseModel):
-    x: int
-    y: int
+    ul_x: int #Координаты левой верхней точки bounding box объекта
+    ul_y: int
+    lr_x: int #Координаты правой нижней точки bounding box объекта
+    lr_y: int
 
 class Storage: #Хранение координат и изображения 
     def __init__(self):
-        self.coordinates = [512, 512]
+        self.coordinates = [0, 0, 1024, 1024]
         self.image = None
+        self.processed_image = None
 
     def set_coords(self, coords: Coordinates):
-        self.coordinates = [coords.x, coords.y]
+        self.coordinates = [coords.ul_x, coords.ul_y, coords.lr_x, coords.lr_y]
     def get_coords(self):
         return self.coordinates
     
@@ -24,8 +30,14 @@ class Storage: #Хранение координат и изображения
     def get_image(self):
         return self.image
     
+    def set_processed_image(self, image: Image):
+        self.processed_image = image
+        
+    def get_processed_image(self):
+        return self.processed_image
 
 img_coord_storage = Storage()
+segmenter = ImageSegmenter()
 
 @app.post('/image')
 async def post_image(image: UploadFile = File(...)):
@@ -60,16 +72,51 @@ async def read_image():
 
 @app.post('/coords')
 async def read_coords(coords: Coordinates):
-    img_coord_storage.update(coords)
-    return {"coordinates": img_coord_storage.get()}
+    img_coord_storage.set_coords(coords)
+    return {"coordinates": img_coord_storage.get_coords()}
 
 @app.get('/coords')
 async def get_coords():
-    return {"coordinates": img_coord_storage.get()}
+    return {"coordinates": img_coord_storage.get_coords()}
 
 @app.get('/test')
 def greet():
  return "Hello World!"
+
+@app.post('/process')
+async def process_image():
+    try:
+        image = img_coord_storage.get_image()
+        coords = img_coord_storage.get_coords()
+        
+        if image is None:
+            return JSONResponse(status_code=400, content={"message": "No image uploaded"})
+            
+        processed_image = segmenter.process_image(image=image, box_prompt=tuple(coords))
+        
+        img_coord_storage.set_processed_image(processed_image)
+        
+        return {"message": "Image processed successfully"}
+        
+    except Exception as err:
+        return JSONResponse(status_code=500, content={"message": f"Processing error: {str(err)}"})
+
+@app.get('/processed-image')
+async def get_processed_image():
+    try:
+        image = img_coord_storage.get_processed_image()
+        if image is None:
+            return JSONResponse(status_code=404, content={"message": "No processed image available"})
+        
+        #Всё ещё помню что нельзя передавать просто PIL
+        img_bytes = BytesIO()
+        image.save(img_bytes, format="PNG")
+        img_bytes.seek(0)
+        
+        return Response(content=img_bytes.read(), media_type="image/png")
+    except Exception as err:
+        return JSONResponse(status_code=500, content={"message": f"Error: {str(err)}"})
+
 
 if __name__ == "__main__":
     import uvicorn
--- image2model/model.py
+++ image2model/model.py
@@ -1,16 +1,36 @@
 from PIL import Image
 from refiners.solutions import BoxSegmenter
+from typing import Tuple, Optional
 
-input_image = Image.open("bg_remove_image.jpg") 
+class ImageSegmenter:
+    def __init__(self):
+        # Downloads the weights from finegrain/finegrain-box-segmenter
+        self.segmenter = BoxSegmenter()
+    
+    # box_prompt is (x_min, y_min, x_max, y_max)
+    def process_image(self, 
+                     image: Image.Image, 
+                     box_prompt: Optional[Tuple[int, int, int, int]] = None) -> Image.Image:
 
-# Downloads the weights from finegrain/finegrain-box-segmenter
-segmenter = BoxSegmenter()
-# box_prompt is (x_min, y_min, x_max, y_max)
-mask = segmenter(input_image, box_prompt=(337, 279, 973, 1024))
+        if image.mode in ("RGBA", "P"):
+            image = image.convert("RGB")
+            
+        mask = self.segmenter(image, box_prompt=box_prompt) if box_prompt else self.segmenter(image)
+        mask = mask.convert("L")
+        transparent_image = Image.new("RGBA", image.size, (0, 0, 0, 0))
+        result_image = Image.composite(image, transparent_image, mask)
+        
+        return result_image
+
+
+
+#input_image = Image.open("bg_remove_image.jpg") 
+#segmenter = BoxSegmenter()
+#mask = segmenter(input_image, box_prompt=(337, 279, 973, 1024))
 # Or without box_prompt as a background remover
 # mask = segmenter(input_image.convert("RGB"))
-mask = mask.convert("L")
-transparent_image = Image.new("RGBA", input_image.size, (0, 0, 0, 0))
-result_image = Image.composite(input_image, transparent_image, mask)
-result_image.save("output.png")
+#mask = mask.convert("L")
+#transparent_image = Image.new("RGBA", input_image.size, (0, 0, 0, 0))
+#result_image = Image.composite(input_image, transparent_image, mask)
+#result_image.save("output.png")
 
